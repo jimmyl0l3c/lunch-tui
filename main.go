9f81dfff7c69eea56d69f3cbfa19599e00a3997f
Filip Joska <filip@joska.dev>
2025-03-12 16:36:56 +0100

    Update timers

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net"
 	"time"
 
+	"github.com/charmbracelet/lipgloss"
 	"github.com/jimmyl0l3c/lunch-tui/menu"
 	"github.com/jimmyl0l3c/lunch-tui/scraper"
 	"github.com/jimmyl0l3c/lunch-tui/styles"
@@ -24,21 +25,7 @@ func getIp() string {
 	return conn.LocalAddr().String()
 }
 
-func refresh() {
-	currentTime := time.Now().Local()
-	weekday := currentTime.Weekday()
-
-	if weekday == time.Sunday || weekday == time.Saturday {
-		fmt.Println(styles.Error("Cannot display menu during weekend"))
-		return
-	}
-
-	if hour := currentTime.Hour(); hour > 12 && weekday != time.Friday {
-		currentTime = currentTime.Add(12 * time.Hour)
-	} else if hour > 12 && weekday == time.Friday {
-		fmt.Println(styles.Url("Tomorow is Friday, nothing to show"))
-	}
-
+func printMenu(currentTime time.Time) {
 	currentDate := currentTime.Format("2.1.")
 
 	restaurants := []menu.Restaurant{
@@ -52,7 +39,32 @@ func refresh() {
 
 func main() {
 	for {
-		refresh()
-		time.Sleep(1 * time.Hour)
+		currentTime := time.Now().Local()
+		weekday := currentTime.Weekday()
+
+		if weekday == time.Sunday || weekday == time.Saturday {
+			fmt.Println(styles.Error("Cannot display menu during weekend"))
+			return
+		}
+
+		noonTime := time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 12, 0, 0, 0, currentTime.Location())
+
+		if hour := currentTime.Hour(); hour >= 12 && weekday != time.Friday {
+			printMenu(currentTime.Add(12 * time.Hour))
+
+			time.Sleep(time.Until(noonTime.Add(24 * time.Hour)))
+		} else if hour >= 12 && weekday == time.Friday {
+			fmt.Println(styles.Url(
+				lipgloss.JoinVertical(lipgloss.Center,
+					"Tomorow is weekend, nothing to show.",
+					"... and what are you doing here anyway?",
+				),
+			))
+			return
+		} else if hour < 12 {
+			printMenu(currentTime)
+
+			time.Sleep(time.Until(noonTime))
+		}
 	}
 }
